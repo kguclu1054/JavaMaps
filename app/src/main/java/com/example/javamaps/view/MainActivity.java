40d827e51d19c10483a149f06113896d4f9da0cc
Kaan Güçlü <kguclu1054@gmail.com>
2025-03-09 13:37:38 +0300

    -second commit

---
+++ app/src/main/java/com/example/javamaps/view/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.javamaps.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.javamaps.adapter.PlaceAdapter;
+import com.example.javamaps.model.Place;
+import java.util.List;
+
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import com.example.javamaps.R;
+import com.example.javamaps.databinding.ActivityMainBinding;
+import com.example.javamaps.roomdb.PlaceDao;
+import com.example.javamaps.roomdb.PlaceDatabase;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    PlaceDatabase db;
+    PlaceDao placeDao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        db = Room.databaseBuilder(getApplicationContext(), PlaceDatabase.class, "Places").build();
+        placeDao = db.placeDao();
+
+        compositeDisposable.add(placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(MainActivity.this::handleResponse)
+        );
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void handleResponse(List<Place> placeList){
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        PlaceAdapter placeAdapter = new PlaceAdapter(placeList);
+        binding.recyclerView.setAdapter(placeAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_place){
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            intent.putExtra("info", "new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.travel_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+
+}
---
+++ app/src/main/java/com/example/javamaps/view/MapsActivity.java
@@ -0,0 +1,251 @@
+package com.example.javamaps.view;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.room.Insert;
+import androidx.room.Room;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.Manifest;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.javamaps.R;
+import com.example.javamaps.model.Place;
+import com.example.javamaps.roomdb.PlaceDao;
+import com.example.javamaps.roomdb.PlaceDatabase;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.example.javamaps.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    ActivityResultLauncher<String> permissionLauncher;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    SharedPreferences sharedPreferences;
+    boolean info;
+    PlaceDatabase db;
+    PlaceDao placeDao;
+    Double selectedLatitude;
+    Double selectedLongitude;
+    Place selectedPlace;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        registerLauncher();
+
+        sharedPreferences = this.getSharedPreferences("com.example.javamaps", MODE_PRIVATE);
+        info = false;
+
+        db = Room.databaseBuilder(getApplicationContext(), PlaceDatabase.class, "Places").build();
+        placeDao = db.placeDao();
+
+        selectedLatitude = 0.0;
+        selectedLongitude = 0.0;
+
+        binding.saveButton.setEnabled(false);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
+
+        Intent intent = getIntent();
+        String Intentİnfo = intent.getStringExtra("info");
+
+        if(Intentİnfo.equals("new")){
+
+            binding.saveButton.setVisibility(View.VISIBLE);
+            binding.deleteButton.setVisibility(View.GONE);
+
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    info = sharedPreferences.getBoolean("info", false);
+
+                    if (!info) {
+                        // Alınan yeni konumu haritaya yerleştir
+                        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12));
+                        mMap.addMarker(new MarkerOptions().position(userLocation).title("Bulunduğunuz Konum").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                        sharedPreferences.edit().putBoolean("info", true).apply();
+                    }
+                }
+
+            };
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Snackbar.make(binding.getRoot(), "Permission needed for maps", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Permission isteği
+                            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                        }
+                    }).show();
+                } else {
+                    // Permission isteği
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                }
+            } else {
+                // Konum güncellemelerini al
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                // İlk başta mevcut konumu almayı deneyin
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (lastLocation != null) {
+                    LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 12));
+                    mMap.addMarker(new MarkerOptions().position(lastUserLocation).title("Bulunduğunuz Konum").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                } else {
+                    // Eğer lastLocation null ise, GPS verisi almak için birkaç saniye bekle
+                    Toast.makeText(MapsActivity.this, "Getting location...", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else{
+
+            mMap.clear();
+
+            selectedPlace = (Place) intent.getSerializableExtra("place");
+
+            LatLng latLng = new LatLng(selectedPlace.latitude , selectedPlace.longitude);
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title(selectedPlace.name));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+
+            binding.placeNameText.setText(selectedPlace.name);
+            binding.saveButton.setVisibility(View.GONE);
+            binding.deleteButton.setVisibility(View.VISIBLE);
+
+        }
+}
+
+    private void registerLauncher() {
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    // Permission verildi
+                    if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                        // Konum güncellemeleri ile cihazın son bilinen konumunu almak
+                        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (lastLocation != null) {
+                            LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 12));
+                        } else {
+                            // Eğer konum verisi alınamazsa, GPS verisi almak için bir süre bekleyin
+                            Toast.makeText(MapsActivity.this, "Getting location...", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    // İzin verilmediği durum
+                    Toast.makeText(MapsActivity.this, "Permission Needed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng));
+
+        selectedLatitude = latLng.latitude;
+        selectedLongitude = latLng.longitude;
+
+        binding.saveButton.setEnabled(true);
+
+    }
+
+    public void save(View view){
+
+        Place place = new Place(binding.placeNameText.getText().toString(),selectedLatitude, selectedLongitude);
+
+
+        //placeDao.insert(place).subscribeOn(Schedulers.io()).subscribe();
+
+        compositeDisposable.add(placeDao.insert(place)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(MapsActivity.this::handleResponse));
+
+    }
+
+    private void handleResponse(){
+        Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    public void delete(View view){
+
+        if (selectedPlace != null){
+            compositeDisposable.add(placeDao.delete(selectedPlace)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(MapsActivity.this::handleResponse)
+            );
+
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+}
